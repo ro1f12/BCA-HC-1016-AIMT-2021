cfe95bc0ff9a5d67081e8af9495e8476387c72a6
Masud Alam Rofi <masud.alam.rofi@gmail.com>
2021-12-11 20:22:56 +0530

    Create template.c

---
+++ template.c
@@ -0,0 +1,24 @@
+/**
+*	this file shows the structure of a C-program file 
+*/
+
+#include<stdio.h>					//<-------system defined header file(s)
+#include"custom.h"					//<-------user defined header  file(s)
+#define SAMPLE_MACRO value			//<-------macros
+
+void sample_function(int arg1);		//<-------function declaration(s)
+
+int main()							//<-------the main() function
+{
+	/**
+	* Code inside main() function
+	*/
+	return 0;
+}
+
+void sample_function(int arg1)		//<-------function definition
+{
+	/**
+	* code for the function
+	*/
+}
